1b90876aedd5a76d5767c1ae88fcbe2288bdfb6e
trungtvnd <tranviettrung20011992@gmail.com>
2023-07-24 20:38:11 +0700

    test build

--- src/main/java/trungtv/jenkins/github/Controller.java
+++ src/main/java/trungtv/jenkins/github/Controller.java
@@ -1,7 +1,10 @@
 package trungtv.jenkins.github;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("testJenkins")
 public class Controller {
+
 }
